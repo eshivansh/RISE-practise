01e3457ffa9a9c54ccfbbd36c7d8fef68dc90489
Aditya <as6510647@gmail.com>
2026-08-19 20:24:13 +0530

    updated App.jsx page

--- Desktop/N.P/PROJECT/rise_aditya/react/src/App.jsx
+++ Desktop/N.P/PROJECT/rise_aditya/react/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>Welcome to RISE</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
@@ -26,7 +26,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          Count is {count}
+          Clicked {count} times
         </button>
       </section>
 
